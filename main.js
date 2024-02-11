7290ee01403942948ff0fb02201df04c5e4dc5f5
abraham <abraham.work7@gmail.com>
2024-02-11 09:01:56 -0700

    add button start

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const boxArray = document.getElementById("container-wrapper");
+const startGame = document.getElementById("start-game");
+
 const numCard = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
 
-const randomNumber = () => {
+const showBoard = () => {
 
     var indexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     indexArray = indexArray.sort(function () { return Math.random() - 0.5 });
@@ -11,10 +13,12 @@ const randomNumber = () => {
     for (let i = 0; i < indexArray.length; i++) {
         const element = document.createElement("button");
         element.className = "card"
-        element.innerHTML = numCard[indexArray[i]];
+        element.disabled = true;
+        //element.innerHTML = numCard[indexArray[i]];
         boxArray.appendChild(element);
     }
 }
 
 
-randomNumber();
+
+showBoard();
